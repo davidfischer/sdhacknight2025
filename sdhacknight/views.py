894b10799430314eef767b5be1d1d84afa432c97
David Fischer <djfische@gmail.com>
2025-02-19 20:40:38 -0800

    Janky ASL detection

--- sdhacknight/views.py
+++ sdhacknight/views.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.views.generic import TemplateView
 from eyepop import EyePopSdk
 
@@ -15,15 +16,34 @@ class HomePageView(TemplateView):
 
     template_name = "sdhacknight/index.html"
 
+    def asl_present(self, result):
+        result_str = str(result)
+        if "thumb" in result_str:
+            return True
+
+        return False
+
     def post(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
 
-        if settings.EYEPOP_SECRET_KEY:
+        if settings.EYEPOP_SECRET_KEY and request.FILES["file"]:
             with EyePopSdk.workerEndpoint(
                 pop_id=pop_id, secret_key=settings.EYEPOP_SECRET_KEY
             ) as endpoint:
-                result = endpoint.upload(request.FILES["file"]).predict()
-                context["result"] = result
+                # Write file to disk temporarily
+                fs = FileSystemStorage()
+                fs.save(request.FILES["file"].name, request.FILES["file"])
+                file_path = fs.location + "/" + request.FILES["file"].name
+
+                log.info("Sending image (%s) to eyepop...", file_path)
+                result = endpoint.upload(file_path).predict()
+                log.info("Eyepop Result: %s", result)
+
+                asl_present = self.asl_present(result)
+                msg = "ASL not detected"
+                if asl_present:
+                    msg = "ASL detected!"
+                context["result"] = msg
         else:
             context["result"] = "No file upload detected"
 
